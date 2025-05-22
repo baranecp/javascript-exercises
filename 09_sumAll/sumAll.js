0c0a812dadc24e56bc20cd2f1644253cab352997
baranecp <baranec.dev@gmail.com>
2025-05-22 14:16:54 +0200

    Write function that returns sum of every integer

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(min , max) {
+  if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+  if (min < 0 || max < 0) return "ERROR";
+  if (min > max) {
+    [min, max] = [max, min]
+  }
 
+    let sum = 0;
+    for(let i = min; i <= max; i++) {
+        sum += i
+    }
+    return sum
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
+
+// Write function that takes 2 numbers min and max
+// If min or max is not a number return ERROR
+// If min or max is negative number return ERROR
+// If min > max then we swap values 
+// Loop trough the given numbers
+// Return sum of the given numbers

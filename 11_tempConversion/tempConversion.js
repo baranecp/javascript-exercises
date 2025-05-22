2e96db9e5acdd9003e0e9bbf83ac0a9c7bcb61ec
baranecp <baranec.dev@gmail.com>
2025-05-22 14:52:05 +0200

    Write function to convert temperatures

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  const c = (f-32) * 5/9
+  return Math.round(c *10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  const f = (c * (9/5)) + 32;
+  return Math.round(f *10) / 10
 };
+
+console.log(convertToFahrenheit(73.2))
 
 // Do not edit below this line
 module.exports = {

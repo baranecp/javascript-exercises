bfaded92356e9fabb4ebeb49930319a11e08676d
baranecp <baranec.dev@gmail.com>
2025-05-22 13:45:27 +0200

    Write function that remove arguments from array

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+//Loop trough array and check if arr contains num and if does remove them from arr
+
+
+const removeFromArray = function(arr, ...items) {
+  const indexToDelete = arr.filter(item => !items.includes(item))
+  return indexToDelete
 };
 
 // Do not edit below this line

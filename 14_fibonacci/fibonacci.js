9ce82913d6b5b0508ec40e37f21d110730074164
baranecp <baranec.dev@gmail.com>
2025-05-27 11:51:58 +0200

    Finish exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+ let count
+    if (typeof n !== 'number') {
+        count = parseInt(n)
+    } else {
+        count = n
+    }
 
+    if (count < 0) return "OOPS";
+    if (count == 0) return 0;
+
+if (count < 2) {
+        return count;
+    } else {
+        return fibonacci(count - 1) + fibonacci(count - 2);
+    }
 };
 
 // Do not edit below this line
